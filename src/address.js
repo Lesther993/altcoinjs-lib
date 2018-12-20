96e7d3db916513bd719ddb4e1f88226e0f431c22
Lesther <Lesther993@gmail.com>
2018-12-20 02:03:10 -0400

    a

--- src/address.js
+++ src/address.js
@@ -12,10 +12,13 @@ function fromBase58Check (address) {
 
   // TODO: 4.0.0, move to "toOutputScript"
   if (payload.length < 21) throw new TypeError(address + ' is too short')
-  if (payload.length > 21) throw new TypeError(address + ' is too long')
+  if (payload.length > 22) throw new TypeError(address + ' is too long')
 
-  const version = payload.readUInt8(0)
-  const hash = payload.slice(1)
+  const multibyte = payload.length === 22
+  const offset = multibyte ? 2 : 1
+
+  const version = multibyte ? payload.readUInt16BE(0) : payload.readUInt8(0)
+  const hash = payload.slice(offset)
 
   return { version: version, hash: hash }
 }
@@ -32,11 +35,15 @@ function fromBech32 (address) {
 }
 
 function toBase58Check (hash, version) {
-  typeforce(types.tuple(types.Hash160bit, types.UInt8), arguments)
+  typeforce(types.tuple(types.Hash160bit, types.UInt16), arguments)
 
-  const payload = Buffer.allocUnsafe(21)
-  payload.writeUInt8(version, 0)
-  hash.copy(payload, 1)
+  var multibyte = version > 0xff
+  var size = multibyte ? 22 : 21
+  var offset = multibyte ? 2 : 1
+
+  const payload = Buffer.allocUnsafe(size)
+  multibyte ? payload.writeUInt16BE(version, 0) : payload.writeUInt8(version, 0)
+  hash.copy(payload, offset)
 
   return bs58check.encode(payload)
 }
